cb040c9017def5c5d9b5fb991ce4e98df1b244cc
letz0703 <letz0703@gmail.com>
2022-04-09 17:02:45 +0900

    vanilla react

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,14 @@
-import React, {Component} from "react";
-
+import React, { Component } from "react";
+import "./Hello.css";
 class Hello extends Component {
     render() {
-        return <h1>Hello World</h1>
+        return (
+            <div className="f1 tc">
+                <h1>letz</h1>
+                <p>go</p>
+                <p>{this.props.greeting}</p>
+            </div>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import Hello from "./Hello";
 import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello greeting={"Hello" + "React Ninja"} />);
 reportWebVitals();
