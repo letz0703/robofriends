dca330e918b63610e3a09bafbbb6fe4739508f19
letz <letz0703@gmail.com>
2022-04-10 00:14:04 +0900

    remove unused file and make three robo friends

---
+++ src/robots.js
@@ -0,0 +1,21 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'icanmart',
+        username: 'ic',
+        email:'icanmart@gmagil.com'
+    },
+    {
+        id: 2,
+        name: 'rainskiss',
+        username: 'rk',
+        email:'rainskiss@nate.com'
+    },
+    {
+        id: 3,
+        name: 'letz',
+        username: 'z',
+        email:'letz@letz.co.kr'
+    },
+
+]
