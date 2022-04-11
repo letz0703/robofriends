8581a0389f9d0147671252dffe433609ab8e74d2
letz0703 <letz0703@gmail.com>
2022-04-11 17:06:40 +0900

    use state & narrow down the result

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>Robo Friends</h1>
-            <SearchBox />
-            <CardList robots={robots} />;
-        </div>
-    );
-};
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: "",
+        };
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value });
+
+        // console.log(filteredRobots);
+    };
+
+    render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name
+                .toLowerCase()
+                .includes(this.state.searchfield.toLowerCase());
+        });
+
+        return (
+            <div className="tc">
+                <h1>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />;
+            </div>
+        );
+    }
+}
 
 export default App;
