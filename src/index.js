cc95d699e4d18570dad1f81fa353ac354d2e09de
letz0703 <letz0703@gmail.com>
2022-04-11 14:04:41 +0900

    new App.js & refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import CardList from "./CardList";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CardList robots={robots} />);
+root.render(<App />);
 reportWebVitals();
