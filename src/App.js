be0bdd781dca795179f47e828704e07e96cdaff3
letz0703 <letz0703@gmail.com>
2022-04-11 15:59:11 +0900

    new SearchBox.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
 const App = () => {
     return (
-        <div>
+        <div className="tc">
             <h1>Robo Friends</h1>
+            <SearchBox />
             <CardList robots={robots} />;
         </div>
     );
