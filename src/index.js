3e31fe0c28af3b23a1dc935637f2c7602ded699d
letz0703 <letz0703@gmail.com>
2022-04-11 11:38:26 +0900

    new CardList.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import Card from "./Card";
+import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import {robots} from './robots';
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
-);
+root.render(<CardList robots={robots} />);
 reportWebVitals();
