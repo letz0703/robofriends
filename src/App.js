808c0ee15a610e6dfabb0a31a1dbbe46cd6d39b3
letz <letz0703@gmail.com>
2022-04-12 00:17:53 +0900

    json place holder:fetch / life cycle hook: componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
-
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: "",
         };
     }
 
+    componentDidMount() {
+        // console.log('check')
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json().then(
+            users => this.setState({ robots: users })
+            )
+        );
+    }
+
     onSearchChange = (e) => {
         this.setState({ searchfield: e.target.value });
-
         // console.log(filteredRobots);
     };
 
@@ -24,15 +30,19 @@ class App extends React.Component {
             return robot.name
                 .toLowerCase()
                 .includes(this.state.searchfield.toLowerCase());
-        });
+        })
 
-        return (
-            <div className="tc">
-                <h1 className="f1">Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />;
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />;
+                </div>
+            );
+        }
     }
 }
 
