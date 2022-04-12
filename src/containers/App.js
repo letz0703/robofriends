84c341f45d02acf9b99e8d4138fb93cb764f71be
letz <letz0703@gmail.com>
2022-04-12 23:22:31 +0900

    organize folders & refactor codes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
-import Scroll from "./Scroll";
+import Scroll from "../components/Scroll";
 
 class App extends React.Component {
   constructor() {
@@ -26,16 +26,16 @@ class App extends React.Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
+    const { robots, searchfield } = this.state
+    const filteredRobots = robots.filter((robot) => {
       return robot.name
         .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
-    });
+        .includes(searchfield.toLowerCase());
+    })
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>;
-    } else {
-      return (
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
         <div className="tc">
           <h1 className="f1">Robo Friends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -44,7 +44,6 @@ class App extends React.Component {
           </Scroll>
         </div>
       );
-    }
   }
 }
 
